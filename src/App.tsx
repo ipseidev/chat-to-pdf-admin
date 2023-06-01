c251d1964ad3759bb157860d845e6641a1f39cfc
Nicolas Serra <serranicolas0904@gmail.com>
2023-06-01 14:34:26 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
+
 import React, { useState } from "react";
 import { useMutation } from "react-query";
 import axios from "axios";
